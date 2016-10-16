3c22ad06de772ac192354afd1cc22ce03aedbdbb
GoD139 <bb-business@hotmail.com>
2016-10-16 15:34:12 +0200

    Add files via upload

--- includes/page_start.php
+++ includes/page_start.php
@@ -17,41 +17,41 @@ $Vali = new Validation();
 //item submission
 if(isset($_POST['ItemSubmit']))
 {
-  $Vali->URLValidation($_POST['ItemLink']); //Validate URL
-  $Vali->StringValidation($_POST['ItemName'],4,50); //Validate String
-  $Vali->SelectIntValidation($_POST['ItemCat']);//Validate Select (int)
+       $Vali->URLValidation($_POST['ItemLink']); //Validate URL
+       $Vali->StringValidation($_POST['ItemName'],4,50); //Validate String
+       $Vali->SelectIntValidation($_POST['ItemCat']);//Validate Select (int)
 
-  if($Vali->Count == 0) //check if any errors
-  {
-    $item->add($_POST['ItemName'],$_POST['ItemLink'],$_POST['ItemCat']);
-  }
+       if($Vali->Count == 0) //check if any errors
+       {
+              $item->add($_POST['ItemName'],$_POST['ItemLink'],$_POST['ItemCat']);
+       }
 }
 
 
 //item update
 if(isset($_POST['ItemUpdate']))
 {
-  $Vali->URLValidation($_POST['ItemLink']); //Validate URL
-  $Vali->StringValidation($_POST['ItemName'],4,50); //Validate String
-  $Vali->SelectIntValidation($_POST['ItemCat']);//Validate Select (int)
+       $Vali->URLValidation($_POST['ItemLink']); //Validate URL
+       $Vali->StringValidation($_POST['ItemName'],4,50); //Validate String
+       $Vali->SelectIntValidation($_POST['ItemCat']);//Validate Select (int)
 
-  if($Vali->Count == 0) //check if any errors
-  {
-    $item->update($_GET['IID'],$_POST['ItemName'],$_POST['ItemLink'],$_POST['ItemCat']);
-  }
+       if($Vali->Count == 0) //check if any errors
+       {
+              $item->update($_GET['IID'],$_POST['ItemName'],$_POST['ItemLink'],$_POST['ItemCat']);
+       }
 }
 
 
 //Item Deletion
 if(isset($_POST['ItemDelete']))
 {
-  $item->delete($_POST['ItemID']);
+       $item->delete($_POST['ItemID']);
 }
 
 //item Privatize
 if(isset($_POST['ItemPrivatize']))
 {
-  $item->privatizeItem($_POST['ItemID']);
+       $item->privatizeItem($_POST['ItemID']);
 }
 
 
